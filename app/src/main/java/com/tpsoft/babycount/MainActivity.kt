c8e0e06f37d033cd1f3884ec00b3e35274da9383
tpsumeta <tpsumeta@gmail.com>
2020-04-20 00:28:13 +0700

    add menu

--- app/src/main/java/com/tpsoft/babycount/MainActivity.kt
+++ app/src/main/java/com/tpsoft/babycount/MainActivity.kt
@@ -1,12 +1,11 @@
 package com.tpsoft.babycount
 
 import android.os.Bundle
-import com.google.android.material.snackbar.Snackbar
-import androidx.appcompat.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
-
+import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -14,7 +13,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
-
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
@@ -24,10 +22,8 @@ class MainActivity : AppCompatActivity() {
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         return when (item.itemId) {
+            R.id.action_history -> true
             R.id.action_settings -> true
             else -> super.onOptionsItemSelected(item)
         }
